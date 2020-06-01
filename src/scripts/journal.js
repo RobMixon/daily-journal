0eb56da70857c54bc66d97232cabcb03cc711729
Rob Mixon <mixonroberth@gmail.com>
2020-05-31 20:09:38 -0500

    messed around with java but didnt get anywhere

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,9 +1,26 @@
-Const journalEntry = {
-    journalDate: Date,
-    conceptsCovered: "",
-    journalEntry: "",
-    mood: "",
+var journalEntry = {
+    journalDate: 05/29/2020,
+    conceptsCovered: "css",
+    journalEntry: "learned CSS",
+    mood: "happy",
+};
+var journalEntryOne = {
+    journalDate: 05/30/2020,
+    conceptsCovered: "html",
+    journalEntry: "learned html",
+    mood: "happy",
+};
+
+var journalEntryTwo = {
+    journalDate: 05/31/2020,
+    conceptsCovered: "Javascript",
+    journalEntry: "learned javascript",
+    mood: "happy",
 };
 
 let entryArray = [];
 
+    entryArray.push(journalEntry);
+    entryArray.push(journalEntryOne);
+    entryArray.push(journalEntryTwo);
+    console.log(entryArray)
